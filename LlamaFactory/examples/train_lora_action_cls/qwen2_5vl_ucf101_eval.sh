14c865c31879fe0ccbaa1d295793fe7e3014bfa8
Lwt235 <m15955295202@163.com>
2026-02-10 22:39:20 +0800

    update

--- LlamaFactory/examples/train_lora_action_cls/qwen2_5vl_ucf101_eval.sh
+++ LlamaFactory/examples/train_lora_action_cls/qwen2_5vl_ucf101_eval.sh
@@ -1,4 +1,4 @@
-export CUDA_VISIBLE_DEVICES=${CUDA_VISIBLE_DEVICES:-5}
+export CUDA_VISIBLE_DEVICES=4
 export HF_HUB_OFFLINE=1
 
 llamafactory-cli train examples/train_lora_action_cls/qwen2_5vl_ucf101_eval.yaml
